35da3fd8a05d3ae2a0489ac9dff840f143f3fc0a
Jayson Custodio <jcustodio23@yahoo.com>
2024-03-25 17:56:11 +0800

    commited

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const openBtn = document.querySelector("main > i");
 const closeBtn = document.querySelector(".nav-sideBar > i");
-
 const navMenu = document.querySelector(".nav-sideBar");
 
 openBtn.addEventListener("click", () => {
