17f19a1e65be7f6fa6c727b70d5b3821fe720a2a
Jayson Custodio <jcustodio23@yahoo.com>
2024-03-25 11:50:25 +0800

    f

---
+++ script.js
@@ -0,0 +1,12 @@
+const openBtn = document.querySelector("main > i");
+const closeBtn = document.querySelector(".nav-sideBar > i");
+
+const navMenu = document.querySelector(".nav-sideBar");
+
+openBtn.addEventListener("click", () => {
+  navMenu.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+  navMenu.classList.remove("active");
+});
